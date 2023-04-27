05ef9eddf0a18c498d7f9b13575c5852ca09bbd0
Ilhombek <ubaydullaevilhombek681@gmail.com>
2023-04-28 04:48:54 +0500

    second

--- src/main/java/com/example/google_cloud/controller/HealthController.java
+++ src/main/java/com/example/google_cloud/controller/HealthController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class HealthController {
 
+    
     @GetMapping("/health")
     public String health() {
         return "hello world";
